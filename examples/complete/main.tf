8dfb3c0dd6780ff2080bd4311ab6f56de4396b34
Matt C <mattc@berkeleypayment.com>
2023-01-12 11:39:25 -0500

    add example for environment

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,20 +1,26 @@
 module "simple" {
   source = "../.."
-  
-  stage = var.stage
+
+  stage     = var.stage
   namespace = var.namespace
-  service = var.service
-  
-  bucket_filter_suffix ="file.csv"
+  service   = var.service
+
+  bucket_filter_suffix = "file.csv"
 
   handler_filename = "index.handler"
-  function_name = "s3test"
+  function_name    = "s3test"
 
-  random_keeper_id = "dsadsa"
-  bucket_name = "test"
-  lambda_src_path = "${path.cwd}/../src/"
-   enable_bucket_trigger = false
-   enable_rds_s3import = true
+  random_keeper_id      = "dsadsa"
+  bucket_name           = "test"
+  lambda_src_path       = "${path.cwd}/../src/"
+  enable_bucket_trigger = false
+  enable_rds_s3import   = true
+
+  environment = {
+    variables = {
+      REGION = "us-east-1"
+    }
+  }
 }
 
 
@@ -26,13 +32,13 @@ resource "random_string" "default" {
 
 
 resource "aws_s3_bucket" "default" {
-  bucket        = "${var.namespace}-${var.stage}-${var.service}-${random_string.default.result}"
+  bucket = "${var.namespace}-${var.stage}-${var.service}-${random_string.default.result}"
 }
 
 
 resource "aws_s3_bucket_public_access_block" "default" {
-  
-  bucket =  aws_s3_bucket.default.bucket 
+
+  bucket = aws_s3_bucket.default.bucket
 
   block_public_acls       = true
   block_public_policy     = true
@@ -41,7 +47,7 @@ resource "aws_s3_bucket_public_access_block" "default" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-  bucket =  aws_s3_bucket.default.bucket 
+  bucket = aws_s3_bucket.default.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
--- examples/complete/providers.tf
+++ examples/complete/providers.tf
@@ -1,15 +1,14 @@
 terraform {
 
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    profile = "development"
+  region  = "us-east-1"
+  profile = "development"
 }
-
