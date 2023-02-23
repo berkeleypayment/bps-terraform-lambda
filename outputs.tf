8277ed0a183f5490f7991f06c4628370d588d3a9
Matt C <mattc@berkeleypayment.com>
2023-02-23 15:08:20 -0500

    add role as output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "s3_bucket" {
 output "lambda_arn" {
   value = aws_lambda_function.default.arn
 }
+
+output "role_name" {
+  value = aws_iam_role.lambda.name
+}
+
+output "role_arn" {
+  value = aws_iam_role.lambda.arn
+}
